93a79f53f009f2b5ca085095a517f53f2aa6bae1
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-03 12:27:14 +0545

    add dark and light theme

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:online_store/config/router/app_routes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class App extends StatelessWidget {
+import '../config/router/app_routes.dart';
+import '../config/themes/app_themes.dart';
+import 'common/provider/is_dark_theme.dart';
+
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkTheme = ref.watch(isDarkThemeProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
+      theme: isDarkTheme ? AppThemes.appDarkTheme() : AppThemes.appLightTheme(),
       routes: AppRoutes.getAppRoutes(),
       initialRoute: AppRoutes.splashRoute,
     );
