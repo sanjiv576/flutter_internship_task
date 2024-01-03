59c4a57e575b9adc4c1bdd25b699e00c3a0771ee
sanjiv576 <shresthasanjiv576@gmail.com>
2024-01-03 12:31:44 +0545

    add entity of shopping cart

--- lib/features/shopping/domain/entity/shopping_entity.dart
+++ lib/features/shopping/domain/entity/shopping_entity.dart
@@ -1,24 +1,44 @@
 import 'package:equatable/equatable.dart';
 
-import '../../../product/domain/entity/product_entity.dart';
+class Cart {
+  final String productId;
+  final String productName;
+  final int quantity;
+  final double amount;
 
-class ShoppingCartEntity extends Equatable {
-  final String? id;
-  final List<ProductEntity> proudctList;
-  final double totalAmount;
-
-  const ShoppingCartEntity(
-      { this.id, required this.proudctList, required this.totalAmount});
-
-  @override
-  List<Object?> get props => [
-        id,
-        totalAmount,
-        proudctList,
-      ];
+  Cart({
+    required this.productId,
+    required this.productName,
+    required this.quantity,
+    required this.amount,
+  });
 
   @override
   String toString() {
-    return 'Shopping id: $id, Product list: $proudctList, total amount: $totalAmount';
+    return 'Product id: $productId, Product name: $productName, Quantity: $quantity, Amount: $amount';
+  }
+}
+
+class ShoppingCartEntity extends Equatable {
+  final String? id;
+  // final List<ProductEntity> proudctList;
+  final List<Cart> cart;
+  double totalAmount;
+  // final int quantity;
+
+  ShoppingCartEntity({
+    this.id,
+    // required this.proudctList,
+    required this.cart,
+    required this.totalAmount,
+    // required this.quantity,
+  });
+
+  @override
+  List<Object?> get props => [id, totalAmount, cart, cart];
+
+  @override
+  String toString() {
+    return 'Shopping id: $id, Cart list: $cart, total amount: $totalAmount, ';
   }
 }
